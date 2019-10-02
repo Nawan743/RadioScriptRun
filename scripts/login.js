5190fba7625eaada081a5b45198c92244cbf8fa0
Yure <yurealexandres@gmail.com>
2019-10-02 19:09:56 -0300

    Chamando variaveis dentro do metodo

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,8 @@
-let user = document.getElementById("username");
-let pass = document.getElementById("password");
-let entrar = document.getElementById("btnEntrar");
-
 function validaLogin(){
+    let user = document.getElementById("username");
+    let pass = document.getElementById("password");
+    let entrar = document.getElementById("btnEntrar");
+
     if(user.value == '' || pass.value == ''){
         entrar.disabled;
         alert("Usuário e/ou senha inválido.");
