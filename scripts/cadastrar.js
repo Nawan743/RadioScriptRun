4db950705207012c31de7e7e56b52f17f13f4a38
Yure <yurealexandres@gmail.com>
2019-10-11 14:27:41 -0300

    Dei uma melhoradinha no css
    Sim eu mexi com o css

--- scripts/cadastrar.js
+++ scripts/cadastrar.js
@@ -5,9 +5,10 @@ function cadastro(){
     let confirmPass = document.getElementById("confirmPassword").value;
     let email = document.getElementById("email").value;
     let localBase = JSON.parse(localStorage.getItem("usuarios")); 
+    // let regex = new RegExp("[A-z0-9]{6}");
     if(user == ""){
         alert("Digite o usuario");
-    }else if(pass == ""){
+    }else if(pass == "" /*&& regex.test(pass)*/){
         alert("Digite sua senha");
     }else if(confirmPass == ""){
         alert("Confirme sua senha");
@@ -15,11 +16,13 @@ function cadastro(){
         alert("Digite seu email");
     }else if (pass != confirmPass){
         alert("As senhas nao conferem, por favor verifique!")
-    }
+    }else {
     if(localBase != null){
         usuarios = localBase;
     }
     usuarios.push({usuario: user, senha: pass, email: email});
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    window.location.href='login.html';
+    }
 
 }
