07cddc1668c255d1a9d29a4b4b07fabd0b601e26
YureSoares <55850877+YureSoares@users.noreply.github.com>
2019-10-22 23:41:18 -0300

    Linkei js ao index
    1 -  Linkar os script na nova página OK
    2 -  Colocar aqueles ícones dentro do input OK
    3 -  Usar o flexbox pra centralizar (SEI FAZER ISSO NAO AMANHA PROCURO)
    4 -  Colocar o background image (TA BUGADO QUANDO APERTA F12)
    5 -  Testar (OQ EU MARQUEI COMO OK EU TESTEI)

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,67 @@ function trocarDivs() {
         document.getElementById("login").style.display = "none";
     }
 }
+function validaLogin(){
+    let user = document.getElementById("username");
+    let pass = document.getElementById("password");
+    let entrar = document.getElementById("btnEntrar");
+
+    if(user.value == '' || pass.value == ''){
+        //entrar.disabled;
+        alert("Usuário e/ou senha inválido.");
+    }
+}
+
+function userIcon(){
+    document.getElementById("username").style.backgroundPosition = "right";
+    document.getElementById("username").style.transition = "all 0.3s";
+}
+
+function passwordIcon(){
+    document.getElementById("password").style.backgroundPosition = "right";
+    document.getElementById("password").style.transition = "all 0.3s";
+}
+function cadastro(){
+    let usuarios = [];
+    let user = document.getElementById("usernameCadastro");
+    let pass = document.getElementById("passwordCadastro");
+    let confirmPass = document.getElementById("confirmPassword");
+    let email = document.getElementById("email");
+    let localBase = JSON.parse(localStorage.getItem("usuarios")); 
+
+    user.style.borderBottom = "2px solid black";
+    pass.style.borderBottom = "2px solid black";
+    confirmPass.style.borderBottom = "2px solid black";
+    email.style.borderBottom = "2px solid black";
+    
+    // let regex = new RegExp("[A-z0-9]{6}");
+    if(user.value == ""){
+        alert("Digite o usuario");
+        user.style.borderBottom = "2px solid red";
+    }else if(pass.value == "" /*&& regex.test(pass)*/){
+        alert("Digite sua senha");
+        pass.style.borderBottom = "2px solid red";
+    }else if(confirmPass.value == ""){
+        alert("Confirme sua senha");
+        confirmPass.style.borderBottom = "2px solid red";
+    }else if(email.value == ""){
+        alert("Digite seu email");  
+        email.style.borderBottom = "2px solid red";
+    }else if (pass.value != confirmPass.value){
+        alert("As senhas nao conferem, por favor verifique!");
+        pass.style.borderBottom = "2px solid red";
+        confirmPass.style.borderBottom = "2px solid red";
+
+    }else {
+    if(localBase != null){
+        usuarios = localBase;
+    }
+    usuarios.push({usuario: user.value, senha: pass.value, email: email.value});
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    document.getElementById("login").style.display = "inherit";
+    document.getElementById("cadastro").style.display = "none";
+    document.getElementById("loginCheck").checked = true;
+    document.getElementById("cadastroCheck").checked = false;
+    }
+
+}
