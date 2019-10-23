bef3c76f37208a072cfde68c4aa3609b83c6eadd
Nawan <49466380+Nawan743@users.noreply.github.com>
2019-10-23 18:45:48 -0200

    alinhando o main e ajustando js

--- scripts/index.js
+++ scripts/index.js
@@ -50,24 +50,53 @@ function cadastro(){
     }else if(confirmPass.value == ""){
         alert("Confirme sua senha");
         confirmPass.style.borderBottom = "2px solid red";
-    }else if(email.value == ""){
-        alert("Digite seu email");  
-        email.style.borderBottom = "2px solid red";
     }else if (pass.value != confirmPass.value){
         alert("As senhas nao conferem, por favor verifique!");
         pass.style.borderBottom = "2px solid red";
         confirmPass.style.borderBottom = "2px solid red";
-
+    }else if(email.value == ""){
+        alert("Digite seu email");  
+        email.style.borderBottom = "2px solid red";
     }else {
-    if(localBase != null){
-        usuarios = localBase;
-    }
-    usuarios.push({usuario: user.value, senha: pass.value, email: email.value});
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
-    document.getElementById("login").style.display = "inherit";
-    document.getElementById("cadastro").style.display = "none";
-    document.getElementById("loginCheck").checked = true;
-    document.getElementById("cadastroCheck").checked = false;
+        if(localBase != null){
+            usuarios = localBase;
+        }
+        usuarios.push({usuario: user.value, senha: pass.value, email: email.value});
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        document.getElementById("login").style.display = "inherit";
+        document.getElementById("cadastro").style.display = "none";
+        document.getElementById("loginCheck").checked = true;
+        document.getElementById("cadastroCheck").checked = false;
     }
 
+}
+
+function userIcon(id){
+    document.getElementById(id).style.backgroundPosition = "right";
+    document.getElementById(id).style.transition = "all 0.3s";
+}
+
+function escondeUserIcon(id){
+    document.getElementById(id).style.backgroundPosition = "0px 50px";
+    document.getElementById(id).style.transition = "all 0.3s";
+}
+
+function passwordIcon(id){
+    document.getElementById(id).style.backgroundPosition = "right";
+    document.getElementById(id).style.transition = "all 0.3s";
+}
+
+function escondePasswordIcon(id){
+    document.getElementById(id).style.backgroundPosition = "0px 50px";
+    document.getElementById(id).style.transition = "all 0.3s";
+}
+
+function emailIcon(){
+    document.getElementById().style.backgroundPosition = "right";
+    document.getElementById().style.transition = "all 0.3s";
+}
+
+function escondeEmailIcon(){
+    document.getElementById().style.backgroundPosition = "0px 50px";
+    document.getElementById().style.transition = "all 0.3s";
 }
--- scripts/login.js
+++ scripts/login.js
@@ -14,7 +14,18 @@ function userIcon(){
     document.getElementById("username").style.transition = "all 0.3s";
 }
 
+function escondeUserIcon(){
+    document.getElementById("username").style.backgroundPosition = "0px 50px";
+    document.getElementById("username").style.transition = "all 0.3s";
+}
+
 function passwordIcon(){
     document.getElementById("password").style.backgroundPosition = "right";
+    document.getElementById("password").style.transition = "all 0.3s";
+}
+
+
+function escondePasswordIcon(){
+    document.getElementById("password").style.backgroundPosition = "0px 50px";
     document.getElementById("password").style.transition = "all 0.3s";
 }
