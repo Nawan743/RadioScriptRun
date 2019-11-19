889da728a9570b690ef29421304f9fb7f703c252
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-11-19 19:39:23 -0300

    Versão estável para apresentação

--- MVC/Models/Banco.java
+++ MVC/Models/Banco.java
@@ -1,6 +1,5 @@
 package Models;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,8 +8,91 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.util.ArrayList;
 
 public class Banco {
+
+	static {
+		Banco b = new Banco();
+		Player p01 = new Player("p01", 743);
+		try {
+			b.registraPlayer(p01);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p02 = new Player("p02", 9);
+		try {
+			b.registraPlayer(p02);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p03 = new Player("p03", 1543);
+		try {
+			b.registraPlayer(p03);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p04 = new Player("p04", 856);
+		try {
+			b.registraPlayer(p04);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p05 = new Player("p05", 13);
+		try {
+			b.registraPlayer(p05);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p06 = new Player("p06", 32);
+		try {
+			b.registraPlayer(p06);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p07 = new Player("p07", 1543);
+		try {
+			b.registraPlayer(p07);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p08 = new Player("p08", 104);
+		try {
+			b.registraPlayer(p08);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p09 = new Player("p09", 51);
+		try {
+			b.registraPlayer(p09);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p10 = new Player("p10", 97);
+		try {
+			b.registraPlayer(p10);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p11 = new Player("p11", 1543);
+		try {
+			b.registraPlayer(p11);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p12 = new Player("p12", 200);
+		try {
+			b.registraPlayer(p12);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Player p13 = new Player("p13", 345);
+		try {
+			b.registraPlayer(p13);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private LineNumberReader lineCounter;
 
@@ -19,30 +101,33 @@ public class Banco {
 		if (!banco.exists()) {
 			banco.createNewFile();
 		}
+		System.out.println(banco.getAbsolutePath());
 	}
 
 	public void registraPlayer(Player p) throws IOException {
 		String player = p.getNome() + " ; " + p.getSenha() + " ; " + p.getEmail() + " ; " + p.getRank();
 		File banco = new File("banco.txt");
-//		System.out.println(banco.getAbsolutePath());
 		BufferedWriter writer = new BufferedWriter(new FileWriter(banco, true));
 		writer.append(player);
 		writer.newLine();
 		writer.close();
 	}
 
-	public String listaBanco() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader("banco.txt"));
-		String linha = "";
-		String banco = "";
+	public ArrayList<Player> listaBanco() throws IOException {
+		lineCounter = new LineNumberReader(new InputStreamReader(new FileInputStream("banco.txt")));
+		String nextLine = null;
 
-		while (linha != null) {
-			banco += linha.concat("\n");
-			linha = reader.readLine();
+		ArrayList<Player> players = new ArrayList<Player>();
+
+		while ((nextLine = lineCounter.readLine()) != null) {
+			String[] info = nextLine.trim().split(";");
+			Player player = new Player(info[0].trim(), info[1].trim(), info[2].trim(),
+					Integer.parseInt(info[3].trim()));
+
+			players.add(player);
 		}
-
-		reader.close();
-		return banco;
+		lineCounter.close();
+		return players;
 	}
 
 	public boolean PlayerExiste(String playerValidar) throws IOException {
@@ -75,5 +160,5 @@ public class Banco {
 		lineCounter.skip(banco.length());
 		return lineCounter.getLineNumber();
 	}
-	
+
 }
--- MVC/Models/Player.java
+++ MVC/Models/Player.java
@@ -14,6 +14,21 @@ public class Player {
 		this.pontuacao = 0;
 	}
 	
+	public Player(String nome, String senha, String email, int pontuacao) {
+		this.nome = nome;
+		this.senha = senha;
+		this.email = email;
+		this.pontuacao = pontuacao;
+	}
+	
+//	Apenas para testar o banco de dados no Heroku
+	public Player(String nome, Integer score) {
+		this.nome = nome;
+		this.senha = "test";
+		this.email = "test@gmail.com";
+		this.pontuacao = score;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
