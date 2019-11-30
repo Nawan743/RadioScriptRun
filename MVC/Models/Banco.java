3d7d4409e4507b739f2acaf6ece57fb8e2077b0f
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-11-30 17:38:08 -0300

    versao com banco local

--- MVC/Models/Banco.java
+++ MVC/Models/Banco.java
@@ -2,18 +2,47 @@ package Models;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.util.ArrayList;
-
-import services.S3;
 
 public class Banco {
 
-	public ArrayList<Player> buscarDadosBanco() {
-		ArrayList<Player> players = S3.buscarDadosBanco();
+	/*
+	 * public ArrayList<Player> buscarDadosBanco2() { ArrayList<Player> players =
+	 * S3.buscarDadosBanco(); return players; }
+	 */
+
+	public ArrayList<Player> buscarDadosBanco() throws IOException {
+		ArrayList<Player> players = new ArrayList<Player>();
+
+		LineNumberReader lineCounter = new LineNumberReader(new InputStreamReader(new FileInputStream("banco.txt")));
+		String nextLine = null;
+
+		while ((nextLine = lineCounter.readLine()) != null) {
+			String[] info = nextLine.trim().split(";");
+			Player player = new Player(info[0].trim(), info[1].trim(), info[2].trim(),
+					Integer.parseInt(info[3].trim()));
+			players.add(player);
+		}
+		lineCounter.close();
 		return players;
 	}
+
+	/*
+	 * public void enviarDadosBanco(ArrayList<Player> players) throws IOException {
+	 * File banco = new File("banco.txt"); BufferedWriter writer = new
+	 * BufferedWriter(new FileWriter(banco, false));
+	 * 
+	 * for (int i = 0; i < players.size(); i++) {
+	 * writer.append(players.get(i).toString()); writer.newLine();
+	 * System.out.println(players.get(i)); } writer.close();
+	 * 
+	 * S3.salvarDadosBanco(banco); }
+	 */
 
 	public void enviarDadosBanco(ArrayList<Player> players) throws IOException {
 		File banco = new File("banco.txt");
@@ -22,11 +51,11 @@ public class Banco {
 		for (int i = 0; i < players.size(); i++) {
 			writer.append(players.get(i).toString());
 			writer.newLine();
-			System.out.println(players.get(i));
+			// System.out.println(players.get(i));
 		}
 		writer.close();
 
-		S3.salvarDadosBanco(banco);
+		// S3.salvarDadosBanco(banco);
 	}
 
 	public void registraPlayer(Player p) throws IOException {
@@ -35,7 +64,7 @@ public class Banco {
 		enviarDadosBanco(players);
 	}
 
-	public boolean playerExiste(String playerValidar) {
+	public boolean playerExiste(String playerValidar) throws IOException {
 		boolean encontrou = false;
 		ArrayList<Player> players = buscarDadosBanco();
 		for (int i = 0; i < players.size(); i++) {
