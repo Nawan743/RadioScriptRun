3137bb3d00483cec0d7d726f8db0d1c321264083
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2019-12-05 22:43:45 -0300

    Revert "melhoria de codigo"
    This reverts commit ce5a96443421663c911f33ce6d9a8f62f2ef3b99.

--- MVC/Models/Banco.java
+++ MVC/Models/Banco.java
@@ -111,7 +111,7 @@ public class Banco {
 			if (!linha.contains(player.getNome())) {
 				contatos += linha;
 			} else {
-				registraPlayer(player);;
+				contatos += (player.toString());
 			}
 		}
 
