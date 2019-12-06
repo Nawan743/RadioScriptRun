874a47bd2441992bd5ed857219033cff311d0949
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-12-05 23:51:21 -0300

    refatoração da mecanica do jogo

--- src/main/webapp/WEB-INF/jogo.jsp
+++ src/main/webapp/WEB-INF/jogo.jsp
@@ -17,16 +17,25 @@
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="./resources/estilo/menu.css" type="text/css">
+    <!-- <link rel="stylesheet" href="./resources/estilo/menu.css" type="text/css"> -->
     <link rel="stylesheet" href="./resources/estilo/jogo.css" type="text/css">
 
-    <!-- JavaScript -->
-    <script src="./resources/scripts/jogo.js"></script>
 </head>
 <body>
     <c:import url="audio.jsp"></c:import>
+    <div id="topo">
+    	<div id="record_score">
+    		<span>RECORDE</span> <span class="score_label" id="record_score_label">451351</span>
+    	</div>
+    	<div id="current_score">
+    		<span>PONTUAÇÃO</span> <span class="score_label" id="current_score_label">00000</span>
+    	</div>
+    </div>
     <div id="caixaBlocos">
         <div class="boxBoneco" id="idBoneco"></div>
     </div>
+
+    <!-- JavaScript -->
+    <script src="./resources/scripts/jogo.js"></script>
 </body>
 </html>
--- src/main/webapp/resources/scripts/jogo.js
+++ src/main/webapp/resources/scripts/jogo.js
@@ -1,213 +1,333 @@
-// ---------------- Movimento do personagem -------------------------
-let primeiraTecla = true;
-let runOn = true;
-let jumpOn = false;
-
-function running() {
-	runOn = true;
-    jumpOn = false;
-	document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/correndo.gif')";
-	document.getElementById('idBoneco').style.height = "100px";
-	document.getElementById('idBoneco').style.width = "90px";
-	document.getElementById('idBoneco').style.marginTop = "230px";
+const levelConfig = {
+	obstaculos: [
+		{src: 'passaro.gif', position: 'top'},
+		{src: 'barril.png', position: 'down'}
+	],
+	speedJump: 500,
+	speedJumpUp: 15,
+	speedmoveObstacle: 5
 }
-
-function turnedDown() {
-	document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/agaixado.gif')";
-	document.getElementById('idBoneco').style.height = "80px";
-	document.getElementById('idBoneco').style.width = "80px";
-    document.getElementById('idBoneco').style.marginTop = "240px";
-    
-}
-
-function jumped() {
-    document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/pulando.png')";
-    document.getElementById('idBoneco').style.height = "100px";
-	document.getElementById('idBoneco').style.width = "90px";
-    document.getElementById('idBoneco').style.marginTop = "50px";
-    setTimeout(running, 950);
-}
-
-
-
-
-// --------------- Controta os obstáculos ---------------------------
+/*
 const g_obstaculos = [
-	{src: 'passaro.gif', position: 'top'},
-	{src: 'barril.png', position: 'down'},
-	{src: 'morcego.gif', position: 'middle'},
-	{src: 'monstro.gif', position: 'down'}
-];
-const g_cronometros = [1,2,3,4,5,6,7,8,9,10];
+	level1 = [
+		{src: 'passaro.gif', position: 'top'},
+		{src: 'barril.png', position: 'down'}
+	],
+	level2 = [
+		{src: 'passaro.gif', position: 'top'},
+		{src: 'monstro.gif', position: 'down'}
+	],
+	level3 = [
+		{src: 'passaro.gif', position: 'top'},
+		{src: 'passaro.gif', position: 'middle'},
+		{src: 'monstro.gif', position: 'down'},
+		{src: 'barril.png', position: 'down'}
+	]
+];*/
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Orquestra as outras funcoes do jogo: cria um novo bloco a cada intervalo
-// aleatorio que pode variar de 1 a 4 segundos
-// Parâmetros: indiceBloco: inicialmente igual a zero. É incrementado até 10
-// (nro max. de blocos na tela) até 10, depois retorna a zero novamente
-// Retorno: nenhum
-// Exemplo de chamada da função: main(0)
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-function main(indiceBloco = 0) {
-    criarNovoBloco(indiceBloco);
-    setTimeout(main,sortearNro(2,4)*1000,(indiceBloco>9)?0:indiceBloco+1);
-}
+let transition;
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-//Sortear um numero aleatorio no intervalo [a,b]
-//Parâmetros: a: limite inferior do intervalo e b: limite superior do intervalo
-//Retorno: número aleatório sorteado
-//Exemplo de chamada da função: sortear(5,45) -> 30
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-function sortearNro(a,b) {
- return Math.round(Math.random()*(b-a))+a;
-}
+const factoryPlayer = function() {
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Cria um novo bloco dinamicamente e adiciona-o na interface (nro max. de
-// blocos por tela: 10)
-// Parâmetros: indiceBloco: inicialmente igual a zero. É incrementado até 10
-// (nro max. de blocos na tela)
-// Retorno: nenhum
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-function criarNovoBloco(indiceBloco) {
-    let idNovoBloco, cssNovoBloco, novoBloco, leftNovoBloco, caixaBlocos, larguracaixaBlocos, novoParagrafo;
-    let larguraBloco = 60;
-    let alturaBloco = 60;
+	let marginTop;
+	let cronJumpUp;
+	let cronJumpDown;
 
-    // Acessa a caixa de blocos onde os blocos irão movimentar
-    caixaBlocos = window.getComputedStyle(document.getElementById("caixaBlocos"));
-    larguracaixaBlocos = caixaBlocos.width;
+	function await() {
+		transition = false;
+		marginTop = 230;
+		document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/parado.gif')";
+		document.getElementById('idBoneco').style.height = "100px";
+		document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+	}
 
-    // Cria um novo bloco para ser inserido na caixa de blocos
-    idNovoBloco = "bloco"+indiceBloco;
-    console.log(idNovoBloco);
-    novoBloco = document.createElement("div");
-    novoBloco.setAttribute("id",idNovoBloco);
-    novoBloco.setAttribute("class","novoBloco");
-    const numSort = sortearNro(0,g_obstaculos.length-1);
-    if (g_obstaculos[numSort].position == 'top') {
-    	cssNovoBloco = `margin-top: 70px; left:+${parseInt(larguracaixaBlocos)-larguraBloco}px; width:${larguraBloco}px; height:${alturaBloco}px; z-index:${indiceBloco}; background-image:url(./resources/images/${g_obstaculos[numSort].src}); background-size: 100%;`;
-    } else if (g_obstaculos[numSort].position == 'middle') {
-    	cssNovoBloco = `margin-top: 170px; left:+${parseInt(larguracaixaBlocos)-larguraBloco}px; width:${larguraBloco}px; height:${alturaBloco}px; z-index:${indiceBloco}; background-image:url(./resources/images/${g_obstaculos[numSort].src}); background-size: 100%;`;
-    } else {
-    	cssNovoBloco = `margin-top: 270px; left:+${parseInt(larguracaixaBlocos)-larguraBloco}px; width:${larguraBloco}px; height:${alturaBloco}px; z-index:${indiceBloco}; background-image:url(./resources/images/${g_obstaculos[numSort].src}); background-size: 100%;`;    	
-    }
-    novoBloco.setAttribute("style",cssNovoBloco);
-    document.getElementById("caixaBlocos").appendChild(novoBloco);
-    
-    // Cria o texto que ira dentro do novo bloco
-    novoParagrafo = document.createElement("p");
-    novoParagrafo.setAttribute("style","color:white; text-align:center;");
-    document.getElementById(idNovoBloco).appendChild(novoParagrafo);
-    g_cronometros[indiceBloco] = setInterval(movimentarBloco,1,idNovoBloco,indiceBloco);
-}
+	function run() {
+		marginTop = 230;
+		document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/correndo.gif')";
+		document.getElementById('idBoneco').style.height = "100px";
+		document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+	}
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Controle para movimentar cada bloco individualmente na interface da direita
-// ate à esquerda da caixa de blocos
-// Parâmetros: indiceBloco: inicialmente igual a zero. É incrementado até 10
-// (nro max. de blocos na tela)
-// Retorno: nenhum
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-function movimentarBloco(idBloco,indiceBloco) {
-    var bloco, posicaoBloco;
-    bloco = window.getComputedStyle(document.getElementById(idBloco));
-    posicaoBloco = (parseInt(bloco.left)-1);
-    document.getElementById(idBloco).style.left = posicaoBloco+"px";
-    let url = window.location.href;
-    let pasta = window.location.pathname;
-    // Testa detecção de colisão no if
-    if(detectarColisao("idBoneco", idBloco)){
-    	document.getElementById('idBoneco').style.height = "100px";
-    	document.getElementById('idBoneco').style.width = "90px";
-    	document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/morrendo.gif')";
-    	setTimeout( () => clearInterval(g_cronometros[indiceBloco]),1000);
-    	window.open(url.replace(pasta, "/rank"),"_self");
-    }
-    
-    // Bloco tocou a margem esquerda da caixa de blocos. Portanto, para o
-	// cronometro dele e remove-o da interface
-    if (posicaoBloco < 0) { 
-        clearInterval(g_cronometros[indiceBloco]);
-        var item = document.getElementById(idBloco);
-        item.parentNode.removeChild(item);
-    }
-}
+	function turnDown() {
+		marginTop = 250;
+		document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/agaixado.gif')";
+		document.getElementById('idBoneco').style.height = "80px";
+		document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+	}
 
-function detectarColisao(objeto1, objeto2) {
-	//return false;
-	let colidiu = false;
-	let obj1 = document.getElementById(objeto1).getBoundingClientRect();
-	let obj2 = document.getElementById(objeto2).getBoundingClientRect();
+	function jump() {
+		transition = true;
+		marginTop = 230;
+		document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/pulando.png')";
+		document.getElementById('idBoneco').style.height = "100px";
+		document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+		cronJumpUp = setInterval(jumpUp,levelConfig.speedJumpUp);
+	}
 
-	let pontos_obj1 = [ {
-		x : obj1.left,
-		y : obj1.top
-	}, {
-		x : obj1.left + obj1.width,
-		y : obj1.top
-	}, {
-		x : obj1.left + obj1.width,
-		y : obj1.top + obj1.height
-	}, {
-		x : obj1.left,
-		y : obj1.top + obj1.height
-	} ];
-	let pontos_obj2 = [ {
-		x : obj2.left,
-		y : obj2.top
-	}, {
-		x : obj2.left + obj2.width,
-		y : obj2.top
-	}, {
-		x : obj2.left + obj2.width,
-		y : obj2.top + obj2.height
-	}, {
-		x : obj2.left,
-		y : obj2.top + obj2.height
-	} ];
-
-	let indice = 0;
-
-	while (colidiu == false && indice <= 3)
-				(pontos_obj1[indice].x >= obj2.left
-				&& pontos_obj1[indice].x <= obj2.left + obj2.width
-				&& pontos_obj1[indice].y >= obj2.top
-				&& pontos_obj1[indice].y <= obj2.top + obj2.height ||
-				pontos_obj2[indice].x >= obj1.left
-				&& pontos_obj2[indice].x <= obj1.left + obj1.width
-				&& pontos_obj2[indice].y >= obj1.top
-				&& pontos_obj2[indice].y <= obj1.top + obj1.height) ? 
-				colidiu = true : indice++;
-	return colidiu;
-}
-
-// ESCUTA O TECLADO
-function keyDown(event) {
-
-	const keyPressed = event.key
-
-	if ((keyPressed === 'ArrowUp' || keyPressed === 'w' || keyPressed === 'W' || keyPressed === ' ') && primeiraTecla === false) {
-		if (jumpOn === false) {
-			jumpOn = true;
-			runOn = false;
-			setTimeout(jumped, 200);
+	function jumpUp() {
+		if (marginTop > 50) {
+			marginTop -= 10;
+			document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+		} else {
+			clearInterval(cronJumpUp);
+			setTimeout(() => {
+				cronJumpDown = setInterval(jumpDown,levelConfig.speedJumpUp);
+			}, levelConfig.speedJump);
 		}
 	}
 
-	if ((keyPressed === 'ArrowDown' || keyPressed === 's' || keyPressed === 'S') && primeiraTecla === false) {
-		jumpOn = false;
-		runOn = false;
-		turnedDown();
+	function jumpDown() {
+		if (marginTop < 230) {
+			marginTop += 10;
+			document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+		} else {
+			clearInterval(cronJumpDown);
+			transition = false;
+			run();
+		}
 	}
-	
-	if (primeiraTecla) {
-		main();
-		primeiraTecla = false;
-		jumpOn = false;
-		runOn = true;
-		running();
+
+	function kill() {
+		marginTop = 230;
+		document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/morrendo.gif')";
+		setTimeout(() => {
+			document.getElementById('idBoneco').style.backgroundImage = "url('./resources/images/morto_1.png')";
+		}, 700);
+		document.getElementById('idBoneco').style.marginTop = `${marginTop}px`;
+	}
+
+	const player = {
+		await,
+		run,
+		turnDown,
+		jump,
+		kill
+	}
+
+	return player;
+}
+
+const factoryObstacle = function() {
+
+	const obstacle = {
+		addObstacle,
+		removeObstacle,
+		moveObstacle,
+		checkColision
+	}
+
+	function addObstacle(id) {
+
+		const larguraBloco = 60;
+		const alturaBloco = 60;
+		let cssNovoBloco;
+
+		// Acessa a caixa de blocos onde os blocos irĂ£o movimentar
+		let caixaBlocos = window.getComputedStyle(document.getElementById("caixaBlocos"));
+		const larguracaixaBlocos = caixaBlocos.width;
+
+		// Cria um novo bloco para ser inserido na caixa de blocos
+		let novoBloco = document.createElement("div");
+		novoBloco.setAttribute("id",id);
+		novoBloco.setAttribute("class","novoBloco");
+
+		const numSort = sortearNro(0,levelConfig.obstaculos.length-1);
+
+		if (levelConfig.obstaculos[numSort].position == 'top') {
+			cssNovoBloco = `margin-top: 70px; left:+${parseInt(larguracaixaBlocos)-larguraBloco}px; width:${larguraBloco}px; height:${alturaBloco}px; z-index:5; background-image:url(./resources/images/${levelConfig.obstaculos[numSort].src}); background-size: 100%;`;
+		} else if (levelConfig.obstaculos[numSort].position == 'middle') {
+			cssNovoBloco = `margin-top: 170px; left:+${parseInt(larguracaixaBlocos)-larguraBloco}px; width:${larguraBloco}px; height:${alturaBloco}px; z-index:5; background-image:url(./resources/images/${levelConfig.obstaculos[numSort].src}); background-size: 100%;`;
+		} else {
+			cssNovoBloco = `margin-top: 270px; left:+${parseInt(larguracaixaBlocos)-larguraBloco}px; width:${larguraBloco}px; height:${alturaBloco}px; z-index:5; background-image:url(./resources/images/${levelConfig.obstaculos[numSort].src}); background-size: 100%;`;    	
+		}
+
+		novoBloco.setAttribute("style",cssNovoBloco);
+		document.getElementById("caixaBlocos").appendChild(novoBloco);
+		//console.log('addObstacle');
+	}
+
+	function removeObstacle(id) {
+		const item = document.getElementById(id);
+		item.parentNode.removeChild(item);
+		//console.log('removeObstacle');
+		const aux = id.replace('bloco','');
+		clearInterval(listObstacles[aux]);
+	}
+
+	function moveObstacle(id) {
+		return setInterval(moveObstacleLeft,levelConfig.speedmoveObstacle,id);
+	}
+
+	function checkColision(id) {
+		//return false;
+		let colidiu = false;
+		const obj1 = document.getElementById('idBoneco').getBoundingClientRect();
+		const obj2 = document.getElementById(id).getBoundingClientRect();
+
+		const pontos_obj1 = [ {
+			x : obj1.left,
+			y : obj1.top
+		}, {
+			x : obj1.left + obj1.width,
+			y : obj1.top
+		}, {
+			x : obj1.left + obj1.width,
+			y : obj1.top + obj1.height
+		}, {
+			x : obj1.left,
+			y : obj1.top + obj1.height
+		} ];
+
+		const pontos_obj2 = [ {
+			x : obj2.left,
+			y : obj2.top
+		}, {
+			x : obj2.left + obj2.width,
+			y : obj2.top
+		}, {
+			x : obj2.left + obj2.width,
+			y : obj2.top + obj2.height
+		}, {
+			x : obj2.left,
+			y : obj2.top + obj2.height
+		} ];
+
+		let indice = 0;
+
+		while (colidiu == false && indice <= 3) {
+			if (pontos_obj1[indice].x >= obj2.left
+					&& pontos_obj1[indice].x <= obj2.left + obj2.width
+					&& pontos_obj1[indice].y >= obj2.top
+					&& pontos_obj1[indice].y <= obj2.top + obj2.height ||
+					pontos_obj2[indice].x >= obj1.left
+					&& pontos_obj2[indice].x <= obj1.left + obj1.width
+					&& pontos_obj2[indice].y >= obj1.top
+					&& pontos_obj2[indice].y <= obj1.top + obj1.height
+			) {
+				colidiu = true;
+				//game.generateObstacles(0);
+				/*for (i=0; i< listObstacles.lenght; i++) {
+					clearInterval(listObstacles[i]);
+				}*/
+				for (var i = 1; i < 99999; i++) {
+					window.clearInterval(i);
+				}
+				player.kill();
+				game.gameOver();
+
+			} else {
+				indice++;
+			}
+		}
+		return colidiu;
+	}
+
+	function moveObstacleLeft(id) {
+		//console.log(`moveObstacleLeft ${id}`)
+		const bloco = window.getComputedStyle(document.getElementById(id));
+		const posicaoBloco = (parseInt(bloco.left)-1);
+		document.getElementById(id).style.left = `${posicaoBloco}px`;
+		//console.log('move')
+		checkColision(id)
+		if (posicaoBloco < 0) { 
+			removeObstacle(id);
+		}
+	}
+
+	function sortearNro(a,b) {
+		return Math.round(Math.random()*(b-a))+a;
+	}
+
+	return obstacle;
+}
+
+const factoryGame = function() {
+	const game = {
+		startCurrentScore,
+		generateObstacles,
+		gameOver
+	}
+
+	let cronCurrentScore;
+	let current_score = 0;
+
+	function generateObstacles(flag) {
+		console.log('entrou '+flag);
+		if (flag == 1) {
+			const id = listObstacles.length;
+			const obstacle = factoryObstacle();
+			obstacle.addObstacle(`bloco${id}`);
+			listObstacles[id] = obstacle.moveObstacle(`bloco${id}`);
+			setTimeout(generateObstacles,sortearNro(2,4)*1000,1);
+		}
+	}
+
+	function gameOver() {
+		//console.log('GAME OVER');
+		generateObstacles(0);
+		for (i=0; i< listObstacles.lenght; i++) {
+			clearInterval(listObstacles[i]);
+		}
+	}
+
+	function startCurrentScore() {
+		cronCurrentScore = setInterval(updateScore, 100);
+	}
+
+	function updateScore() {
+		current_score++;
+		document.getElementById('current_score_label').innerHTML = pad(current_score, 6);
+	}
+
+	function pad(text, size) {
+		let s = String(text);
+		while (s.length < (size || 2)) {s = "0" + s;}
+		return s;
+	}
+
+	function sortearNro(a,b) {
+		return Math.round(Math.random()*(b-a))+a;
+	}
+
+	return game;
+}
+
+let gameStarted = false;
+const game = factoryGame();
+const player = factoryPlayer();
+player.await();
+
+let listObstacles = [];
+
+function startGame() {
+	gameStarted = true;
+	player.run();
+	game.startCurrentScore();
+	game.generateObstacles(1);
+}
+
+function keyDown(event) {
+
+	const keyPressed = event.key;
+
+	if (
+		(keyPressed === 'ArrowUp' || keyPressed === 'w' || keyPressed === 'W' || keyPressed === ' ')
+		&& gameStarted
+		&& transition === false
+	) {
+		player.jump();
+	}
+
+	if (
+		(keyPressed === 'ArrowDown' || keyPressed === 's' || keyPressed === 'S')
+		&& gameStarted
+		&& transition === false
+	) {
+		player.turnDown();
+	}
+
+	if (!gameStarted) {
+		startGame();
 	}
 }
 
@@ -216,7 +336,9 @@ function keyUp(event) {
 	const keyPressed = event.key
 	
 	if (keyPressed === 'ArrowDown' || keyPressed === 's' || keyPressed === 'S') {
-		setTimeout(running, 200);
+		setTimeout(() => {
+			player.run();
+		}, 200);
 	}
 }
 
