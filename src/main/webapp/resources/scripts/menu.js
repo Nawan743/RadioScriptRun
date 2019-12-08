b52851b9dae8d9ac52f5c4a04949a55e551ed450
Nawan <49466380+Nawan743@users.noreply.github.com>
2019-12-08 15:09:42 -0300

    adicionando nivel como parametro e trocando icone dos niveis

--- src/main/webapp/resources/scripts/menu.js
+++ src/main/webapp/resources/scripts/menu.js
@@ -1,27 +1,37 @@
 let cont = 0;
-let form = document.createElement("form");
+let formFacil = document.createElement("form");
+let formMedio = document.createElement("form");
+let formDificil = document.createElement("form");
 let inputFacil = document.createElement("input");
 let inputMedio = document.createElement("input");
 let inputDificil = document.createElement("input");
 
 function escolherNivel() {
 	if (cont % 2 == 0) {
-		form.setAttribute("id", "jogo");
-		form.setAttribute("action", "jogo");
-		form.setAttribute("method", "POST");
-		document.getElementsByTagName("main")[0].appendChild(form);
+		formFacil.setAttribute("id", "jogoFacil");
+		formFacil.setAttribute("action", "jogo");
+		formMedio.setAttribute("id", "jogoMedio");
+		formMedio.setAttribute("action", "jogo");
+		formDificil.setAttribute("id", "jogoDificl");
+		formDificil.setAttribute("action", "jogo");
+		document.getElementsByTagName("main")[0].appendChild(formFacil);
+		document.getElementsByTagName("main")[0].appendChild(formMedio);
+		document.getElementsByTagName("main")[0].appendChild(formDificil);
 		inputFacil.setAttribute("type", "submit");
-		inputFacil.setAttribute("value", "★");
+		inputFacil.setAttribute("value", "☢️");
 		inputFacil.setAttribute("id", "facil");
+		inputFacil.setAttribute("name", "facil");
 		inputMedio.setAttribute("type", "submit");
-		inputMedio.setAttribute("value", "★★");
-		inputMedio.setAttribute("id", "medio")
+		inputMedio.setAttribute("value", "☢️☢️");
+		inputMedio.setAttribute("id", "medio");
+		inputMedio.setAttribute("name", "medio");
 		inputDificil.setAttribute("type", "submit");
-		inputDificil.setAttribute("value", "★★★");
+		inputDificil.setAttribute("value", "☢️☢️☢️");
 		inputDificil.setAttribute("id", "dificil");
-		document.getElementById("jogo").appendChild(inputFacil);
-		document.getElementById("jogo").appendChild(inputMedio);
-		document.getElementById("jogo").appendChild(inputDificil);
+		inputDificil.setAttribute("name", "dificil");
+		document.getElementById("jogoFacil").appendChild(inputFacil);
+		document.getElementById("jogoMedio").appendChild(inputMedio);
+		document.getElementById("jogoDificil").appendChild(inputDificil);
 		cont++;
 	}
 	else{
