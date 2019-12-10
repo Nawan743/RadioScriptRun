364e677346b3dd76f2821983cfbd83d11bc6f375
Nawan <49466380+Nawan743@users.noreply.github.com>
2019-12-10 16:29:49 -0300

    Adicionando label no menu

--- src/main/webapp/resources/scripts/menu.js
+++ src/main/webapp/resources/scripts/menu.js
@@ -1,5 +1,11 @@
 let cont = 0;
 let form = document.createElement("form");
+let divFacil = document.createElement("div");
+let divMedio = document.createElement("div");
+let divDificil = document.createElement("div");
+let labelFacil = document.createElement("label");
+let labelMedio = document.createElement("label");
+let labelDificil = document.createElement("label");
 let inputFacil = document.createElement("input");
 let inputMedio = document.createElement("input");
 let inputDificil = document.createElement("input");
@@ -10,6 +16,18 @@ function escolherNivel() {
 		form.setAttribute("action", "jogo");
 		form.setAttribute("method", "post");
 		document.querySelector("#botoes").appendChild(form);
+		divFacil.setAttribute("id", "levelFacil");
+		divMedio.setAttribute("id", "levelMedio");
+		divDificil.setAttribute("id", "levelDificil");
+		document.getElementById("jogo").appendChild(divFacil);
+		document.getElementById("jogo").appendChild(divMedio);
+		document.getElementById("jogo").appendChild(divDificil);
+		labelFacil.setAttribute("for", "facil");
+		labelFacil.textContent = "Fácil";
+		labelMedio.setAttribute("for", "medio");
+		labelMedio.textContent = "Médio";
+		labelDificil.setAttribute("for", "dificil");
+		labelDificil.textContent = "Díficil";
 		inputFacil.setAttribute("type", "submit");
 		inputFacil.setAttribute("value", "☢️");
 		inputFacil.setAttribute("id", "facil");
@@ -22,15 +40,24 @@ function escolherNivel() {
 		inputDificil.setAttribute("value", "☢️☢️☢️");
 		inputDificil.setAttribute("id", "dificil");
 		inputDificil.setAttribute("onclick", "level('dificil')");
-		document.getElementById("jogo").appendChild(inputFacil);
-		document.getElementById("jogo").appendChild(inputMedio);
-		document.getElementById("jogo").appendChild(inputDificil);
+		document.getElementById("levelFacil").appendChild(inputFacil);
+		document.getElementById("levelFacil").appendChild(labelFacil);
+		document.getElementById("levelMedio").appendChild(inputMedio);
+		document.getElementById("levelMedio").appendChild(labelMedio);
+		document.getElementById("levelDificil").appendChild(inputDificil);
+		document.getElementById("levelDificil").appendChild(labelDificil);
 		cont++;
 	}
 	else{
-		document.getElementById("jogo").removeChild(inputFacil);
-		document.getElementById("jogo").removeChild(inputMedio);
-		document.getElementById("jogo").removeChild(inputDificil);
+		document.getElementById("levelFacil").removeChild(inputFacil);
+		document.getElementById("levelMedio").removeChild(inputMedio);
+		document.getElementById("levelDificil").removeChild(inputDificil);
+		document.getElementById("levelFacil").removeChild(labelFacil);
+		document.getElementById("levelMedio").removeChild(labelMedio);
+		document.getElementById("levelDificil").removeChild(labelDificil);
+		document.getElementById("jogo").removeChild(divFacil);
+		document.getElementById("jogo").removeChild(divMedio);
+		document.getElementById("jogo").removeChild(divDificil);
 		document.querySelector("#botoes").removeChild(form);
 		cont++;
 	}
